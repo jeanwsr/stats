d92ae5672fd21cb3a18df3cd9c10c6504aa2ef07
jeanwsr <srwang20@fudan.edu.cn>
2023-06-11 21:16:45 +0800

    add plot

--- plot.py
+++ plot.py
@@ -9,12 +9,14 @@ import argparse
 def argument_parse():
     parser=argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
                                description='')
-    parser.add_argument("-i","--input",dest='input',metavar='input',type=str,#default='dump',
-                        required=True, #choices=['dh','scf'],
+    parser.add_argument("input",type=str,#default='dump',
                         help='')
-    parser.add_argument("-x","--show",dest='show',metavar='show',type=int,default=0,
-                        required=False, #choices=['dh','scf'],
+    parser.add_argument("-x","--show", action="store_true",
+                        required=False, 
                         help='')
+    #parser.add_argument("-i","--interp", action="store_true",
+    #                    required=True, 
+    #                    help='')
     args=parser.parse_args()
     return parser, args
 
@@ -45,10 +47,32 @@ f.close()
 x = np.array(x)
 y = np.array(y)
 
-plt.plot(x, y[:,1])
-plt.plot(x, y[:,2])
-plt.plot(x, y[:,3])
-plt.plot(x, y[:,4])
+from scipy.interpolate import make_interp_spline as spl
+from scipy.optimize import root
+def spline(x, y):
+    f = spl(x, y, k=3)
+    deriv = f.derivative()
+    res = root(deriv, x[2])
+    print('root:    %.6f' %res.x)
+    print('y(root): %.6f' %f(res.x))
+    return f, (res.x, f(res.x))
+
+def plot(x, y):
+    samp = np.linspace(x[0], x[-1], 50)
+    func, point = spline(x, y)
+    y_samp = func(samp)
+    #print(samp)
+    l, = plt.plot(samp, y_samp )
+    plt.plot([point[0]], [point[1]], 'ro', markersize=3)
+    return l
+
+plt_lines = []
+for i in range(1,5):
+    l = plot(x, y[:,i])
+    plt_lines.append(l)
+plt.xlabel('R / $\AA$')
+plt.ylabel('E / a.u.')
+plt.legend(handles = plt_lines, labels = ['SU-tPBE', 'SUHF', 'SU-tPBE0', 'SU-tPBE(0.25,2)'])
 if args.show:
     plt.show()
 plt.savefig(datafile+'.png')
